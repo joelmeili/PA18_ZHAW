0099b5c345e54074cc31518268d1eb10e97d6f6b
Joël Meili <joel.meili@gmail.com>
2018-09-26 22:22:38 +0200

    edited read_data file, used a more cleaner approach regarding data handling

--- read_data.R
+++ read_data.R
@@ -1,19 +1,51 @@
+# ----------------------------
+# author: Joël Meili
+# meilijoe@students.zhaw.ch
+# ----------------------------
+
+# - load packages
 library(readxl)
 library(dplyr)
 library(lubridate)
 library(ggplot2)
 library(ggthemes)
 
+# - read data
 X35years <- read_excel("C:/Users/joelm/PA18_ZHAW/35years.xlsx")
 data <- as.data.frame(X35years)
 colnames(data) <- c("Date", "SP500", "DAX30", "USTreas", "UKGilt", "EUR", "JPY", "Gold", "Oil")
+
+# - create dataframes for daily, weekly, monthly and yearly aggregation of returns
 data.long <- data.daily <- data %>% gather(Asset, Value, 2:ncol(data))
 data.weekly <- data.long %>% group_by(Year=year(Date), Week=week(Date), Asset) %>% summarise(Value=sum(Value))
 data.monthly <- data.long %>% group_by(Year=year(Date), Month=month(Date), Asset) %>% summarise(Value=sum(Value))
 data.yearly <- data.long %>% group_by(Year=year(Date), Asset) %>% summarise(Value=sum(Value))
 
-conf.level <- c(0.84, 0.975, 0.999)
+# - calculating value-at-risk for three different quantiles for each category e.g. daily data
+conf.level <- list(0.84, 0.975, 0.999)
 
+daily.var <- lapply(conf.level, FUN=function(x){
+  temp <- data.daily %>% group_by(Asset) %>% summarise(VaR=quantile(Value, p=x))
+  colnames(temp)[2] <- paste("VaR", paste0(x*100, "%"))
+  temp
+}) %>% reduce(inner_join, by="Asset") %>% gather(VaR, Value, 2:(length(conf.level)+1))
+weekly.var <- lapply(conf.level, FUN=function(x){
+  temp <- data.weekly %>% group_by(Asset) %>% summarise(VaR=quantile(Value, p=x))
+  colnames(temp)[2] <- paste("VaR", paste0(x*100, "%"))
+  temp
+}) %>% reduce(inner_join, by="Asset") %>% gather(VaR, Value, 2:(length(conf.level)+1))
+monthly.var <- lapply(conf.level, FUN=function(x){
+  temp <- data.monthly %>% group_by(Asset) %>% summarise(VaR=quantile(Value, p=x))
+  colnames(temp)[2] <- paste("VaR", paste0(x*100, "%"))
+  temp
+}) %>% reduce(inner_join, by="Asset") %>% gather(VaR, Value, 2:(length(conf.level)+1))
+yearly.var <- lapply(conf.level, FUN=function(x){
+  temp <- data.yearly %>% group_by(Asset) %>% summarise(VaR=quantile(Value, p=x))
+  colnames(temp)[2] <- paste("VaR", paste0(x*100, "%"))
+  temp
+}) %>% reduce(inner_join, by="Asset") %>% gather(VaR, Value, 2:(length(conf.level)+1))
+
+# - plot distribution of returns for each category e.g. daily data
 g.daily <- ggplot(data.daily, aes(x=Value))+
   geom_density()+
   facet_wrap(~Asset)+
